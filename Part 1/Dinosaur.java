5241c61f32d03743d648e0511aa802c981be83f5
Lucas Cavalheiro <70550085+CyberLuly@users.noreply.github.com>
2024-02-20 18:47:52 -0300

    Exercises 2-6
    Part 1 solutions for exercises 2 through 6

---
+++ Part 1/Dinosaur.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Dinosaur {
+
+    public static void main(String[] args) {
+        System.out.println("Write a message: ");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        System.out.println(userInput);
+    }
+}
