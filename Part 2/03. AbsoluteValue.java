aeab7b242a7ec720aada6f080307f0e7538ed3d8
Lucas Cavalheiro <70550085+CyberLuly@users.noreply.github.com>
2024-02-20 19:10:14 -0300

    Exercises 1-3
    Solution for exercises 1 through 3 from Part 2.

---
+++ Part 2/03. AbsoluteValue.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class AbsoluteValue {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int firstNumber = Integer.valueOf(scanner.nextLine());
+
+        if (firstNumber < 0) {
+            System.out.println(firstNumber * -1);
+        } else {
+            System.out.println(firstNumber);
+        }
+    }
+}
