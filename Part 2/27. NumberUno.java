69d9c02d2c819892d77bc2f85504fc7d41bfca37
Lucas Cavalheiro <70550085+CyberLuly@users.noreply.github.com>
2024-02-21 17:19:17 -0300

    Exercises 22 - 31
    Solution for exercises 22 through 31 of part 2.

---
+++ Part 2/27. NumberUno.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class NumberUno {
+
+    public static void main(String[] args) {
+        numberUno();
+        
+    }
+
+    public static int numberUno() {
+        return 1;
+
+    }
+}
