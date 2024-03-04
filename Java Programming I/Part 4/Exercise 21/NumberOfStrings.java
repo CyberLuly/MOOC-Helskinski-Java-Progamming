4bbd012a3f54d17dbac2c860ad46d0ab30d474e6
Lucas Cavalheiro <70550085+CyberLuly@users.noreply.github.com>
2024-03-04 15:51:52 -0300

    Exercise 21
    Solution for exercise 21.

---
+++ Java Programming I/Part 4/Exercise 21/NumberOfStrings.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class NumberOfStrings {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int numberOfInputs = 0;
+
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("end")) {
+                break;
+            }
+            numberOfInputs++;
+        }
+
+        System.out.println(numberOfInputs);
+
+
+    }
+}
