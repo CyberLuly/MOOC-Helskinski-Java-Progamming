427d9b96955ab7e2403a1f64aef229c82bf73c01
Lucas Cavalheiro <70550085+CyberLuly@users.noreply.github.com>
2024-03-05 18:16:25 -0300

    Exercise 30
    Solution for exercise 30.

---
+++ Java Programming I/Part 4/Exercise 30/StoringRecords.java
@@ -0,0 +1,40 @@
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StoringRecords {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Filename:");
+        String file = scan.nextLine();
+
+        ArrayList<Person> records = readRecordsFromFile(file);
+        System.out.println("Persons: " + records.size());
+        System.out.println("Persons:");
+        for (Person person : records) {
+            System.out.println(person);
+
+        }
+    }
+
+    public static ArrayList<Person> readRecordsFromFile(String file) {
+        ArrayList<Person> persons = new ArrayList<>();
+        try (Scanner fileReader = new Scanner(Paths.get(file))) {
+
+            while (fileReader.hasNextLine()) {
+                String row = fileReader.nextLine();
+                String[] person = row.split(",");
+                Person newPerson = new Person(person[0], Integer.valueOf(person[1]));
+                persons.add(newPerson);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return persons;
+
+    }
+}
