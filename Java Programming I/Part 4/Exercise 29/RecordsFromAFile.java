e4e6d0c91f23883e2be40450651e866fb8595cf4
Lucas Cavalheiro <70550085+CyberLuly@users.noreply.github.com>
2024-03-05 17:56:29 -0300

    Exercise 29
    Solution for exercise 29.

---
+++ Java Programming I/Part 4/Exercise 29/RecordsFromAFile.java
@@ -0,0 +1,32 @@
+
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class RecordsFromAFile {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Name of the file:");
+        String file = scanner.nextLine();
+
+        try (Scanner fileReader = new Scanner(Paths.get(file))) {
+            while (fileReader.hasNextLine()) {
+                String row = fileReader.nextLine();
+                String[] parts = row.split(",");
+                //Continue if row is empty
+                if (row.isEmpty()) {
+                    continue;
+                }
+                if (Integer.valueOf(parts[1]) == 1) {
+                    System.out.println(parts[0] + ", age: " + parts[1] + " year");
+                } else {
+                    System.out.println(parts[0] + ", age: " + parts[1] + " years");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
+}
