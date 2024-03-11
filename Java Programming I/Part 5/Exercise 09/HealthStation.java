7bd7b4c277a066a712dd17028d2a5b4a39809844
Lucas Cavalheiro <70550085+CyberLuly@users.noreply.github.com>
2024-03-11 17:39:59 -0300

    Exercise 08, 09 and 10
    Solution for exercises 08, 09 and 10.

---
+++ Java Programming I/Part 5/Exercise 09/HealthStation.java
@@ -0,0 +1,20 @@
+
+public class HealthStation {
+
+    private int numberOfWeighings;
+
+
+    public int weigh(Person person) {
+        numberOfWeighings++;
+        return person.getWeight();
+    }
+
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
+    }
+
+    public int weighings() {
+        return numberOfWeighings;
+    }
+
+}
