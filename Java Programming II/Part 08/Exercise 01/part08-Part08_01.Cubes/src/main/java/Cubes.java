0cf83282fe345d87de1b58df1086538fb0641f5f
Lucas Cavalheiro <70550085+CyberLuly@users.noreply.github.com>
2024-04-08 15:57:32 -0300

    Exercise 01
    Solution for exercise 01.

--- Java Programming II/Part 08/Exercise 01/part08-Part08_01.Cubes/src/main/java/Cubes.java
+++ Java Programming II/Part 08/Exercise 01/part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -6,5 +6,19 @@ public class Cubes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("Write a number ('end' to exit)");
+            String command = scanner.nextLine();
+            if (command.equals("end")) {
+                break;
+            }
+            System.out.println(cubed(Integer.valueOf(command)));
+
+        }
     }
+
+    public static int cubed(int number) {
+        return number * number * number;
+    }
+
 }
