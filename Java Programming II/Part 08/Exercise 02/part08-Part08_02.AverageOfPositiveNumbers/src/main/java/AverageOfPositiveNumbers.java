0a8fc3fc7fdb1831f52f8b4736c2becd68c2620e
Lucas Cavalheiro <70550085+CyberLuly@users.noreply.github.com>
2024-04-08 16:10:06 -0300

    Exercise 02
    Solution for exercise 02.

--- Java Programming II/Part 08/Exercise 02/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ Java Programming II/Part 08/Exercise 02/part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -1,10 +1,37 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        while (true) {
+
+            String userInput = scanner.nextLine();
+
+            if (Integer.valueOf(userInput) == 0) {
+                break;
+            }
+
+            if (Integer.valueOf(userInput) > 0) {
+                numbers.add(Integer.valueOf(userInput));
+            }
+
+        }
+
+        int sum = 0;
+        for (int number : numbers) {
+            sum = sum + number;
+        }
+        if (sum > 0) {
+            System.out.println((double) sum / numbers.size());
+        } else {
+            System.out.println("Cannot calculate the average");
+        }
 
     }
 }
